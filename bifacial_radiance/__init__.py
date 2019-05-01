27d1628dbb5a3972d05da10a06a5814b233b49a9
cdeline <chris.deline@nrel.gov>
2019-05-01 17:51:15 -0600

    rename bifacial_radiance.py to main_py.

--- bifacial_radiance/__init__.py
+++ bifacial_radiance/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import 
-from bifacial_radiance.bifacial_radiance import AnalysisObj, GroundObj, MetObj, RadianceObj, SceneObj
+from bifacial_radiance.main import AnalysisObj, GroundObj, MetObj, RadianceObj, SceneObj
 from bifacial_radiance.readepw import readepw
 from bifacial_radiance import load
