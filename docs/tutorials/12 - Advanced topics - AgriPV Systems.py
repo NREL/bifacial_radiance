6fdc4055bdf44482047e66151ad7befdccf457e9
shirubana <silvanaa@email.arizona.edu>
2020-10-18 13:28:14 -0300

    AgriPV Mapping

--- docs/tutorials/12 - Advanced topics - AgriPV Systems.py
+++ docs/tutorials/12 - Advanced topics - AgriPV Systems.py
@@ -5,7 +5,7 @@
 # 
 # This journal shows how to model an AgriPV site, calculating the irradiance not only on the modules but also the irradiance received by the ground to evaluate available solar ersource for plants. 
 # 
-# We assume that bifacia_radiacne is already installed in yoru computer. This works for bifacial_radiance v.3 release.
+# We assume that bifacia_radiacne is already installed in your computer. This works for bifacial_radiance v.3 release.
 # 
 # These journal outlines 4 useful uses of bifacial_radiance and some tricks: 
 # 
@@ -16,7 +16,18 @@
 #     <li> Adding object to simulate variations in ground albedo from different crops between rows. </li>
 # </ul>
 # 
-# This is what we will create:
+# 
+# #### Steps:
+# 
+# <ol>
+#     <li> <a href='#step1'> Generate the geometry </a></li>
+#     <li> <a href='#step2'> Analyse the Ground Irradiance </a></li>
+#     <li> <a href='#step3'> Analyse and MAP the Ground Irradiance </a></li>
+#     <li> <a href='#step4'> Adding different Albedo Section </a></li>
+# </ol>
+#     
+# #### Preview of what we will create: 
+#     
 # ![Another view](../images_wiki/AdvancedJournals/AgriPV_2.PNG)
 # ![AgriPV Image We will create](../images_wiki/AdvancedJournals/AgriPV_1.PNG)
 # And this is how it will look like:
@@ -24,35 +35,46 @@
 # ![AgriPV modeled step 4](../images_wiki/AdvancedJournals/AgriPV_step4.PNG)
 # 
 # 
-# ### Steps:
-# 
-# <ol>
-#     <li> <a href='#step1'> Setup of Variables through Making OCT Axis </a></li>
-#     <li> <a href='#step2'> Adding the pillars </a></li>
-#     <li> <a href='#step3'> Adding the different albedo regions </a></li>
-#     <li> <a href='#step4'> Mapping the ground irradiance undernet the PV panel </a></li>
-# <ol>
 # 
 
 # <a id='step1'></a>
 
-# ### 1. Setup of Variables through Making OCT Axis
+# ## 1. Generate the geometry 
+# 
+# This section goes from setting up variables to making the OCT axis. We are also adding some custom elements for the torquetubes and posts.
 # 
 # We've done this before a couple times, no new stuff here. 
 # 
-# The magic is that, for doing the carport we see in the figure, we are going to do a 4-up configuration of modules (**numpanels**), and we are going to repeat that 4-UP 7 times (**nMods**)
+# The magic is that, for doing the carport we see in the figure, we are going to do a 4-up configuration of modules (**numpanels**), and we are going to repeat that 3-UP 6 times (**nMods**)
 
-# In[1]:
+# In[5]:
+
+
+import os
+from pathlib import Path
+
+testfolder = str(Path().resolve().parent.parent / 'bifacial_radiance' / 'TEMP' / 'AgriPVDemo')
+
+# Another option using relative address; for some operative systems you might need '/' instead of '\'
+# testfolder = os.path.abspath(r'..\..\bifacial_radiance\TEMP')  
+
+try:
+    os.stat(testfolder)
+except:
+    os.mkdir(testfolder)
+    
+print ("Your simulation will be stored in %s" % testfolder)
+
+
+# In[6]:
 
 
 from bifacial_radiance import *   
 import numpy as np
 
 
-# In[2]:
+# In[9]:
 
-
-testfolder = r'C:\Users\sayala\Documents\RadianceScenes\Demo4'
 
 timestamp = 4020 # Noon, June 17th. 
 simulationname = 'AgriPV'
@@ -92,7 +114,7 @@ pitch = 15 # m
 albedo = 0.2  #'grass'     # ground albedo
 hub_height = 4.3 # m  
 nMods = 6 # six modules per row.
-nRows = 2  # only 1 row
+nRows = 3  # 3 row
 
 azimuth_ang=180 # Facing south
 tilt =35 # tilt. 
@@ -125,15 +147,13 @@ octfile = demo.makeOct(demo.getfilelist())  # makeOct combines all of the ground
 # ![AgriPV modeled step 1](../images_wiki/AdvancedJournals/AgriPV_step1.PNG)
 # 
 
-# <a id='step2'></a>
-
-# ### 2. Adding the structure
+# ### Adding the structure
 # 
 # We will add on the torquetube and pillars.
 # 
 # Positions of the piles could be done more programatically, but they are kinda estimated at the moment. 
 
-# In[5]:
+# In[10]:
 
 
 torquetubelength = moduleDict['scenex']*(nMods) 
@@ -150,10 +170,15 @@ text='! genbox Metal_Aluminum_Anodized torquetube_row2 {} 0.2 0.3 | xform -t {} 
 customObject = demo.makeCustomObject(name,text)
 demo.appendtoScene(radfile=scene.radfiles, customObject=customObject, text="!xform -rz 0")
 
+name='Post3'
+text='! genbox Metal_Aluminum_Anodized torquetube_row2 {} 0.2 0.3 | xform -t {} -0.1 -0.3 | xform -t 0 -15 4.2'.format(torquetubelength, (-torquetubelength+moduleDict['sceney'])/2.0)
+customObject = demo.makeCustomObject(name,text)
+demo.appendtoScene(radfile=scene.radfiles, customObject=customObject, text="!xform -rz 0")
+
 #octfile = demo.makeOct()  # makeOct combines all of the ground, sky and object files into a .oct file.
 
 
-# In[6]:
+# In[11]:
 
 
 name='Pile'
@@ -162,10 +187,12 @@ pilesep = pile1x*2.0/7.0
 #! genrev Metal_Grey tube1 t*1.004 0.05 32 | xform -ry 90 -t -0.502 0 0
 text= '! genrev Metal_Grey tube1row1 t*4.2 0.15 32 | xform -t {} 0 0'.format(pile1x)
 text += '\r\n! genrev Metal_Grey tube1row2 t*4.2 0.15 32 | xform -t {} 15 0'.format(pile1x)
+text += '\r\n! genrev Metal_Grey tube1row3 t*4.2 0.15 32 | xform -t {} -15 0'.format(pile1x)
 
 for i in range (1, 7):
     text += '\r\n! genrev Metal_Grey tube{}row1 t*4.2 0.15 32 | xform -t {} 0 0'.format(i+1, pile1x-pilesep*i)
     text += '\r\n! genrev Metal_Grey tube{}row2 t*4.2 0.15 32 | xform -t {} 15 0'.format(i+1, pile1x-pilesep*i)
+    text += '\r\n! genrev Metal_Grey tube{}row3 t*4.2 0.15 32 | xform -t {} -15 0'.format(i+1, pile1x-pilesep*i)
 
 customObject = demo.makeCustomObject(name,text)
 demo.appendtoScene(radfile=scene.radfiles, customObject=customObject, text="!xform -rz 0")
@@ -185,33 +212,11 @@ octfile = demo.makeOct()  # makeOct combines all of the ground, sky and object f
 
 # <a id='step3'></a>
 
-# ### 3. Adding different Albedo Sections
-# Add a surface (just like we added the pillars) with a specific reflectivity to represent different albedo sections. In the image, we can see that the albedo between the crops is different than the crop albedo. Let's assume that the abledo between the crops is higher than the crop's albedo which wa previuosly set a 0.2.
-# 
-# 
-
-# In[16]:
-
-
-name='Center_Grass'
-carpositionx=-2
-carpositiony=-1
-text='! genbox white_EPDM CenterPatch 28 12 0.1 | xform -t -14 2 0'.format(carpositionx, carpositiony)
-customObject = demo.makeCustomObject(name,text)
-demo.appendtoScene(scene.radfiles, customObject, '!xform -rz 0')
-
-octfile = demo.makeOct(demo.getfilelist())  # makeOct combines all of the ground, sky and object files into a .oct file.
-
-
-# Viewing with rvu:
-# 
-# ![AgriPV modeled step 4](../images_wiki/AdvancedJournals/AgriPV_step4.PNG)
-# 
-# 
-
 # <a id='step4'></a>
 
-# ### 4. Analysis of the Ground Irradiance
+# <a id='step2'></a>
+
+# ## 2. Analyse  the Ground Irradiance
 # 
 # Now let's do some analysis along the ground, starting from the edge of the modules. We wil select to start in the center of the array.
 # 
@@ -236,7 +241,7 @@ groundscan = frontscan
 
 groundscan['zstart'] = 0.05  # setting it 5 cm from the ground.
 groundscan['zinc'] = 0   # no tilt necessary. 
-groundscan['yinc'] = pitch/(sensorsy-1)   # no tilt necessary. 
+groundscan['yinc'] = pitch/(sensorsy-1)   # increasing spacing so it covers all distance between rows
 groundscan
 
 
@@ -246,25 +251,133 @@ groundscan
 analysis.analysis(octfile, simulationname+"_groundscan", groundscan, backscan)  # compare the back vs front irradiance  
 
 
-# In[20]:
+# This is the result for only one 'chord' accross the ground. Let's now do a X-Y scan of the ground.
+
+# <a id='step3'></a>
+
+# ## 3. Analyse and MAP the Ground Irradiance
+
+#  We will use the same technique to find the irradiance on the ground used above, but will move it along the X-axis to map from the start of one module to the next.
+#  
+#  We will sample around the module that is placed at the center of the field.
+
+# ![AgriPV modeled step 4](../images_wiki/AdvancedJournals/spacing_between_modules.PNG)
+
+# In[55]:
 
 
-analysis.Wm2Front
+import pandas as pd
+import seaborn as sns
 
 
-# # TO ADD: MAP more underneat the module, load results and make colormap.
+# In[16]:
+
+
+sensorsx = 20
+startgroundsample=-moduleDict['scenex']
+spacingbetweensamples = moduleDict['scenex']/(sensorsx-1)
+
+for i in range (0, sensorsx): # Will map 20 points    
+    frontscan, backscan = analysis.moduleAnalysis(scene, sensorsy=sensorsy)
+    groundscan = frontscan
+    groundscan['zstart'] = 0.05  # setting it 5 cm from the ground.
+    groundscan['zinc'] = 0   # no tilt necessary. 
+    groundscan['yinc'] = pitch/(sensorsy-1)   # increasing spacing so it covers all distance between rows
+    groundscan['xstart'] = startgroundsample + i*spacingbetweensamples   # increasing spacing so it covers all distance between rows
+    analysis.analysis(octfile, simulationname+"_groundscan_"+str(i), groundscan, backscan)  # compare the back vs front irradiance  
+
+
+# Read all the files generated into one dataframe
+
+# In[30]:
+
+
+filestarter = "irr_AgriPV_groundscan_"
+
+filelist = sorted(os.listdir(os.path.join(testfolder, 'results')))
+prefixed = [filename for filename in filelist if filename.startswith(filestarter)]
+arrayWm2Front = []
+arrayWm2Back = []
+arrayMatFront = []
+arrayMatBack = []
+filenamed = []
+faillist = []
+
+print('{} files in the directory'.format(filelist.__len__()))
+print('{} groundscan files in the directory'.format(prefixed.__len__()))
+i = 0  # counter to track # files loaded.
+
+for i in range (0, len(prefixed)):
+    ind = prefixed[i].split('_')
+    #print(" Working on ", filelist[i], locs[ii], Scenario[jj])
+    try:
+        resultsDF = load.read1Result(os.path.join(testfolder, 'results', prefixed[i]))
+        arrayWm2Front.append(list(resultsDF['Wm2Front']))
+        arrayWm2Back.append(list(resultsDF['Wm2Back']))
+        arrayMatFront.append(list(resultsDF['mattype']))
+        arrayMatBack.append(list(resultsDF['rearMat']))
+        filenamed.append(prefixed[i])
+    except:
+        print(" FAILED ", i, prefixed[i])
+        faillist.append(prefixed[i])
+
+resultsdf = pd.DataFrame(list(zip(arrayWm2Front, arrayWm2Back, 
+                                  arrayMatFront, arrayMatBack)),
+                         columns = ['br_Wm2Front', 'br_Wm2Back', 
+                                    'br_MatFront', 'br_MatBack'])
+resultsdf['filename'] = filenamed
+
+
+# Creating a new dataframe where  each element in the front irradiance list is a column. Also transpose and reverse so it looks like a top-down view of the ground.
+
+# In[54]:
+
+
+df3 = pd.DataFrame(resultsdf['br_Wm2Front'].to_list())
+reversed_df = df3.T.iloc[::-1]
+
+
+# In[59]:
+
+
+sns.set(rc={'figure.figsize':(11.7,8.27)})
+
+
+# In[60]:
+
+
+# Plot
+ax = sns.heatmap(reversed_df)
+ax.set_yticks([])
+ax.set_xticks([])
+ax.set_ylabel('')  
+ax.set_xlabel('')  
+#ax.tick_params(left=False, bottom=False)
+print('')
+
 
 # <a id='step4'></a>
 
-# <div class="alert alert-warning">
-# This is a note. 
-# 
-# </div>
+# ## 4. Adding different Albedo Sections
+# Add a surface (just like we added the pillars) with a specific reflectivity to represent different albedo sections. In the image, we can see that the albedo between the crops is different than the crop albedo. Let's assume that the abledo between the crops is higher than the crop's albedo which wa previuosly set a 0.2.
 # 
 # 
 
-# In[ ]:
+# In[16]:
 
 
+name='Center_Grass'
+carpositionx=-2
+carpositiony=-1
+text='! genbox white_EPDM CenterPatch 28 12 0.1 | xform -t -14 2 0'.format(carpositionx, carpositiony)
+customObject = demo.makeCustomObject(name,text)
+demo.appendtoScene(scene.radfiles, customObject, '!xform -rz 0')
+
+octfile = demo.makeOct(demo.getfilelist())  # makeOct combines all of the ground, sky and object files into a .oct file.
 
 
+# Viewing with rvu:
+# 
+# ![AgriPV modeled step 4](../images_wiki/AdvancedJournals/AgriPV_step4.PNG)
+# 
+# 
