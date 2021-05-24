934a81fa444508670e45120bd1714d30d7fde6db
Silvana Ayala <silvana.ayala@nrel.gov>
2021-05-24 10:20:54 -0600

    update spectra_utils

--- bifacial_radiance/spectral_utils.py
+++ bifacial_radiance/spectral_utils.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
-
+import pySMARTS
 from collections.abc import Iterable
+import bifacial_radiance as br
+
 
 class spectral_property(object):
     def load_file(filepath):
@@ -137,13 +139,9 @@ class spectral_property(object):
     
 def spectral_albedo_smarts(zen, azm, material, min_wavelength=300,
                            max_wavelength=4000):
-    try:
-        from pySMARTS.main import SMARTSSpectraZenAzm
-    except:
-        print("Warning: Could not load pySMARTS module.")
-        return None
+
     
-    smarts_res = SMARTSSpectraZenAzm('30 31', str(zen), str(azm), material,
+    smarts_res = pySMARTS.SMARTSSpectraZenAzm('30 31', str(zen), str(azm), material,
                                      min_wvl=str(min_wavelength),
                                      max_wvl=str(max_wavelength))
     
@@ -152,13 +150,9 @@ def spectral_albedo_smarts(zen, azm, material, min_wavelength=300,
 
 def spectral_irradiance_smarts(zen, azm, min_wavelength=300,
                            max_wavelength=4000):
-    try:
-        from pySMARTS.main import SMARTSSpectraZenAzm
-    except:
-        print("Warning: Could not load pySMARTS module.")
-        return None
+
     
-    smarts_res = SMARTSSpectraZenAzm('2 3 4', str(zen), str(azm),
+    smarts_res = pySMARTS.SMARTSSpectraZenAzm('2 3 4', str(zen), str(azm),
                                      min_wvl=str(min_wavelength),
                                      max_wvl=str(max_wavelength))
     
@@ -181,14 +175,8 @@ def spectral_irradiance_smarts_SRRL(YEAR, MONTH, DAY, HOUR, ZONE,
                                 RHOG, material,
                                 IOUT='2 3 4', min_wvl='280', max_wvl='4000'):
     
-
-    try:
-        from pySMARTS.main import SMARTSSRRL
-    except:
-        print("Warning: Could not load pySMARTS module.")
-        return None
     
-    smarts_res = SMARTSSRRL(IOUT=IOUT, YEAR=YEAR,MONTH=MONTH,DAY=DAY,HOUR=HOUR, ZONE=ZONE,
+    smarts_res = pySMARTS.SMARTSSRRL(IOUT=IOUT, YEAR=YEAR,MONTH=MONTH,DAY=DAY,HOUR=HOUR, ZONE=ZONE,
                             LATIT=LATIT, LONGIT=LONGIT, ALTIT=ALTIT, 
                              RH=RH, TAIR=TAIR, SEASON=SEASON, TDAY=TDAY, SPR=SPR, W=W, 
                              TILT=TILT, WAZIM=WAZIM, HEIGHT=HEIGHT,
@@ -217,13 +205,8 @@ def spectral_albedo_smarts_SRRL(YEAR, MONTH, DAY, HOUR, ZONE,
                                 RHOG, material,
                                 IOUT='30 31', min_wvl='280', max_wvl='4000'):
  
-    try:
-        from pySMARTS.main import SMARTSSRRL
-    except:
-        print("Warning: Could not load pySMARTS module.")
-        return None
-
-    smarts_res = SMARTSSRRL(IOUT=IOUT, YEAR=YEAR,MONTH=MONTH,DAY=DAY,HOUR=HOUR, ZONE=ZONE,
+ 
+    smarts_res = pySMARTS.SMARTSSRRL(IOUT=IOUT, YEAR=YEAR,MONTH=MONTH,DAY=DAY,HOUR=HOUR, ZONE=ZONE,
                             LATIT=LATIT, LONGIT=LONGIT, ALTIT=ALTIT, 
                              RH=RH, TAIR=TAIR, SEASON=SEASON, TDAY=TDAY, SPR=SPR, W=W, 
                              TILT=TILT, WAZIM=WAZIM, HEIGHT=HEIGHT,
@@ -235,4 +218,90 @@ def spectral_albedo_smarts_SRRL(YEAR, MONTH, DAY, HOUR, ZONE,
     return spectral_property(smarts_res['Zonal_ground_reflectance'],
                              smarts_res['Wvlgth'], interpolation='linear')
     
+
+def gen_spectra(idx=None, metdata=None, spectra_folder=None, scale_spectra=False,
+                scale_albedo=False, scale_albedo_nonspectral_sim=False ):
     
+    # Extract data from metdata
+    dni = metdata.dni[idx]
+    dhi = metdata.dhi[idx]
+    ghi = metdata.ghi[idx]
+    alb = metdata.albedo[idx]
+    solpos = metdata.solpos.iloc[idx]
+    zen = float(solpos.zenith)
+    azm = float(solpos.azimuth) - 180
+    #lat = metdata.latitude
+    #lon = metdata.longitude
+    #elev = metdata.elevation / 1000
+    #t = metdata.datetime[idx]
+
+    # Verify sun up
+    if zen > 90:
+        print("Sun below horizon. Skipping.")
+        return None
+    
+    # Define file suffix
+    # -- CHANGE --
+    suffix = f'_{idx:04}.txt'
+    
+    # Generate/Load dni and dhi
+    dni_file = os.path.join(spectra_folder, "dni"+suffix)
+    dhi_file = os.path.join(spectra_folder, "dhi"+suffix)
+    ghi_file = os.path.join(spectra_folder, "ghi"+suffix)
+    spectral_dni, spectral_dhi, spectral_ghi = br.spectral_utils.spectral_irradiance_smarts(zen, azm, min_wavelength=280)
+    
+    # If specifed, scale the irradiance spectra based on their respective
+    # measured value.
+    if scale_spectra:
+        
+        dni_scale = dni / spectral_dni.df.apply(lambda g: integrate.trapz(spectral_dni.df.value, x=spectral_dni.df.index))
+        dhi_scale = dhi / spectral_dhi.df.apply(lambda g: integrate.trapz(spectral_dhi.df.value, x=spectral_dhi.df.index))
+        ghi_scale = ghi / spectral_ghi.df.apply(lambda g: integrate.trapz(spectral_ghi.df.value, x=spectral_ghi.df.index))
+        # dni_scale = dni / (10*np.sum(spectral_dni[range(280, 4000, 10)]))
+        # dhi_scale = dhi / (10*np.sum(spectral_dhi[range(280, 4000, 10)]))
+        # ghi_scale = ghi / (10*np.sum(spectral_ghi[range(280, 2501, 10)]))
+        spectral_dni.scale_values(dni_scale.value)
+        spectral_dhi.scale_values(dhi_scale.value)
+        spectral_ghi.scale_values(ghi_scale.value)
+    
+    # Write irradiance spectra
+    #'''
+    spectral_dni.to_file(dni_file)
+    spectral_dhi.to_file(dhi_file)
+    spectral_ghi.to_file(ghi_file)
+    #'''
+    
+    # Generate/Load albedo
+    alb_file = os.path.join(spectra_folder, "alb"+suffix)
+    spectral_alb = br.spectral_utils.spectral_albedo_smarts(zen, azm, 'Gravel', min_wavelength=280)
+ 
+    # If specifed, scale the spectral albedo to have a mean value matching the
+    # measured albedo.
+    if scale_albedo:
+        # option A
+        denom = spectral_alb.df.value * spectral_ghi.df.value
+        # option B
+        #denom = spectral_alb.df
+
+        # TODO:
+        # Add test to
+        if alb > 1 or alb == 0:
+            print("albedo measured is an incorrect number, not scaling albedo generated")
+        else:
+            alb_scale = alb / denom.apply(lambda g: integrate.trapz(denom.values, x=spectral_alb.df.index))
+            spectral_alb.scale_values(alb_scale.values)
+
+    if scale_albedo_nonspectral_sim:
+        sim_alb = np.sum(spectral_alb[range(280, 2501, 10)] * spectral_ghi[range(280, 2501, 10)])/np.sum(spectral_ghi[range(280, 2501, 10)])
+        
+        if alb > 1:
+            print("albedo measured is an incorrect number, not scaling albedo generated")
+        else:
+            alb_scale = alb / sim_alb
+            spectral_alb.scale_values(alb_scale)
+            print(alb, sim_alb, alb_scale)
+    
+    # Write albedo file
+    spectral_alb.to_file(alb_file)
+    
+    return (spectral_alb, spectral_dni, spectral_dhi)
