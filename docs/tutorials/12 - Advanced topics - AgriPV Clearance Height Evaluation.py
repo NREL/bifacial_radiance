8d21ebda23dc05d2d3dd29a8b926ada42a2e9c1c
cdeline <chris.deline@nrel.gov>
2021-10-30 08:41:42 -0600

    Notebook12

--- docs/tutorials/12 - Advanced topics - AgriPV Clearance Height Evaluation.py
+++ docs/tutorials/12 - Advanced topics - AgriPV Clearance Height Evaluation.py
@@ -30,7 +30,7 @@ except:
 print ("Your simulation will be stored in %s" % testfolder)
 
 
-# In[7]:
+# In[2]:
 
 
 from bifacial_radiance import *   
@@ -38,7 +38,7 @@ import numpy as np
 import datetime
 
 
-# In[8]:
+# In[3]:
 
 
 startdt_tomato =datetime.datetime(2001,3,15,0)
@@ -49,7 +49,7 @@ startdt_PV = datetime.datetime(2001,1,1,0)
 enddt_PV = datetime.datetime(2001,12,31,23)
 
 
-# In[9]:
+# In[4]:
 
 
 hub_heights = [4.3, 3.5, 2.5, 1.5]
@@ -57,7 +57,7 @@ albedo = 0.2  #'grass'     # ground albedo
 crops = ['tomato', 'kale']
 
 # Redundant. Overwritihng the Radiance Obj for each loop below to have a unique name.
-demo = RadianceObj('getEPW_notused',path = testfolder)  # Create a RadianceObj 'object'
+demo = RadianceObj('getEPW_notused', path=testfolder)  # Create a RadianceObj 'object'
 demo.setGround(albedo) # input albedo number or material name like 'concrete'.  To see options, run this without any input.
 lat = 32.22  # Tucson, AZ
 lon = -110.97  # Tucson, Az 32.2226° N, 110.9747° W
@@ -66,7 +66,7 @@ epwfile = demo.getEPW(lat, lon) # NJ lat/lon 40.0583° N, 74.4057
 
 # ## 1. Loop over the different heights
 
-# In[11]:
+# In[5]:
 
 
 for jj in range (0, len(hub_heights)):
@@ -116,32 +116,33 @@ for jj in range (0, len(hub_heights)):
 
     #demo.gendaylit(4020)  # Use this to simulate only one hour at a time. 
     # Making module with all the variables
-    moduleDict=demo.makeModule(name=moduletype,x=x,y=y,numpanels = numpanels, xgap=xgap, ygap=ygap, cellLevelModuleParams = cellLevelModuleParams)
+    module = demo.makeModule(name=moduletype, x=x, y=y, numpanels=numpanels, 
+                               xgap=xgap, ygap=ygap, cellModule=cellLevelModuleParams)
     # create a scene with all the variables
-    sceneDict = {'tilt':tilt,'pitch': 15,'hub_height':hub_height,'azimuth':azimuth_ang, 'module_type':moduletype, 'nMods': nMods, 'nRows': nRows}  
-    scene = demo.makeScene(moduletype=moduletype, sceneDict=sceneDict) #makeScene creates a .rad file with 20 modules per row, 7 rows.
+    sceneDict = {'tilt':tilt, 'pitch':15, 'hub_height':hub_height, 'azimuth':azimuth_ang, 'nMods':nMods, 'nRows':nRows}  
+    scene = demo.makeScene(module=moduletype, sceneDict=sceneDict) #makeScene creates a .rad file with 20 modules per row, 7 rows.
     octfile = demo.makeOct(demo.getfilelist())  # makeOct combines all of the ground, sky and object fil|es into a .oct file.
 
-    torquetubelength = moduleDict['scenex']*(nMods) 
+    torquetubelength = module.scenex*(nMods) 
 
     # torquetube 1
     name='Post1'
-    text='! genbox Metal_Aluminum_Anodized torquetube_row1 {} 0.2 0.3 | xform -t {} -0.1 -0.3 | xform -t 0 0 {}'.format(torquetubelength, (-torquetubelength+moduleDict['sceney'])/2.0, torquetube_height)
+    text='! genbox Metal_Aluminum_Anodized torquetube_row1 {} 0.2 0.3 | xform -t {} -0.1 -0.3 | xform -t 0 0 {}'.format(torquetubelength, (-torquetubelength+module.sceney)/2.0, torquetube_height)
     customObject = demo.makeCustomObject(name,text)
     demo.appendtoScene(radfile=scene.radfiles, customObject=customObject, text="!xform -rz 0")
 
     name='Post2'
-    text='! genbox Metal_Aluminum_Anodized torquetube_row2 {} 0.2 0.3 | xform -t {} -0.1 -0.3 | xform -t 0 15 {}'.format(torquetubelength, (-torquetubelength+moduleDict['sceney'])/2.0, torquetube_height)
+    text='! genbox Metal_Aluminum_Anodized torquetube_row2 {} 0.2 0.3 | xform -t {} -0.1 -0.3 | xform -t 0 15 {}'.format(torquetubelength, (-torquetubelength+module.sceney)/2.0, torquetube_height)
     customObject = demo.makeCustomObject(name,text)
     demo.appendtoScene(radfile=scene.radfiles, customObject=customObject, text="!xform -rz 0")
 
     name='Post3'
-    text='! genbox Metal_Aluminum_Anodized torquetube_row2 {} 0.2 0.3 | xform -t {} -0.1 -0.3 | xform -t 0 -15 {}'.format(torquetubelength, (-torquetubelength+moduleDict['sceney'])/2.0, torquetube_height)
+    text='! genbox Metal_Aluminum_Anodized torquetube_row2 {} 0.2 0.3 | xform -t {} -0.1 -0.3 | xform -t 0 -15 {}'.format(torquetubelength, (-torquetubelength+module.sceney)/2.0, torquetube_height)
     customObject = demo.makeCustomObject(name,text)
     demo.appendtoScene(radfile=scene.radfiles, customObject=customObject, text="!xform -rz 0")
 
     name='Pile'
-    pile1x = (torquetubelength+moduleDict['sceney'])/2.0
+    pile1x = (torquetubelength+module.sceney)/2.0
     pilesep = pile1x*2.0/7.0
     #! genrev Metal_Grey tube1 t*1.004 0.05 32 | xform -ry 90 -t -0.502 0 0
     text= '! genrev Metal_Grey tube1row1 t*{} 0.15 32 | xform -t {} 0 0'.format(torquetube_height, pile1x)
@@ -162,27 +163,27 @@ for jj in range (0, len(hub_heights)):
         if crop == 'tomato':
             #startdt =datetime.datetime(2001,3,15,0)
             #enddt = datetime.datetime(2001,6,30,23)
-            starttime = '03_15_01' 
-            endtime = '06_30_23'
+            starttime = '2001-03-15_0100' 
+            endtime = '2001-06-30_2300'
             print('PLANTING TOMATO')
 
         if crop == 'kale':
 #            startdt = datetime.datetime(2001,9,15,0)
 #            enddt = datetime.datetime(2001,10,31,23)
-            starttime = '09_15_01' 
-            endtime = '10_31_23'
+            starttime = '2001-09-15_0100' 
+            endtime = '2001-10-31_2300'
             print('PLANTING KALE')
         
-        metdata = demo.readEPW(epwfile, starttime = starttime, endtime = endtime) # read in the EPW weather data from above
-        demo.genCumSky(savefile = crop)#startdt=startdt, enddt=enddt)
+        metdata = demo.readWeatherFile(epwfile, starttime=starttime, endtime=endtime, coerce_year=2001) # read in the EPW weather data from above
+        demo.genCumSky(savefile=crop)
 
         octfile = demo.makeOct()  # makeOct combines all of the ground, sky and object files into a .oct file.
 
         analysis = AnalysisObj(octfile, demo.name)  # return an analysis object including the scan dimensions for back irradiance
         sensorsy = 30
         sensorsx = 30
-        startgroundsample=-moduleDict['scenex']
-        spacingbetweensamples = moduleDict['scenex']/(sensorsx-1)
+        startgroundsample=-module.scenex
+        spacingbetweensamples = module.scenex/(sensorsx-1)
 
         for i in range (0, sensorsx):  
             frontscan, backscan = analysis.moduleAnalysis(scene, sensorsy=sensorsy)
@@ -193,7 +194,7 @@ for jj in range (0, len(hub_heights)):
             groundscan['xstart'] = startgroundsample + i*spacingbetweensamples   # increasing spacing so it covers all distance between rows
             analysis.analysis(octfile, simulationname+'_'+crop+'_'+str(i), groundscan, backscan)  # compare the back vs front irradiance  
 
-    metdata = demo.readEPW(epwfile) # read in the EPW weather data from above
+    metdata = demo.readWeatherFile(epwfile) # read in the EPW weather data from above
     demo.genCumSky(savefile = 'PV')#startdt=startdt, enddt=enddt)
 
     octfile = demo.makeOct()  # makeOct combines all of the ground, sky and object files into a .oct file.
@@ -201,8 +202,8 @@ for jj in range (0, len(hub_heights)):
     analysis = AnalysisObj(octfile, demo.name)  # return an analysis object including the scan dimensions for back irradiance
     sensorsy = 20
     sensorsx = 12
-    startPVsample=-moduleDict['x']
-    spacingbetweenPVsamples = moduleDict['x']/(sensorsx-1)
+    startPVsample=-module.data['x']
+    spacingbetweenPVsamples = module.data['x']/(sensorsx-1)
 
     for i in range (0, sensorsx): # Will map 20 points    
         frontscan, backscan = analysis.moduleAnalysis(scene, sensorsy=sensorsy)
@@ -213,7 +214,7 @@ for jj in range (0, len(hub_heights)):
 
 # ## 2. Plot Bifacial Gain Results
 
-# In[58]:
+# In[6]:
 
 
 import pandas as pd
@@ -222,7 +223,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 
-# In[45]:
+# In[7]:
 
 
 font = {'family' : 'normal',
@@ -233,14 +234,14 @@ matplotlib.rc('font', **font)
 sns.set(rc={'figure.figsize':(11.7,8.27)})
 
 
-# In[39]:
+# In[9]:
 
 
 hub_heights = [4.3, 3.5, 2.5, 1.5]
 results_BGG=[]
 for i in range(0, len(hub_heights)):
     hub_height = str(int(hub_heights[i]*100))
-    filepv= r'C:\Users\Silvana\Documents\GitHub\bifacial_radiance\bifacial_radiance\TEMP\AgriPVStudy\results\irr_height_{}cm_PV_6.csv'.format(hub_height)
+    filepv= os.path.join(testfolder,'results',f'irr_height_{hub_height}cm_PV_6.csv')
     resultsDF = load.read1Result(filepv)
     resultsDF = load.cleanResult(resultsDF).dropna()
     results_BGG.append(resultsDF['Wm2Back'].sum()*100/resultsDF['Wm2Front'].sum())
@@ -251,7 +252,7 @@ plt.ylabel('Bifacial Gain in Irradiance (BG$_G$) [%]')
 plt.xlabel('Hub height [m]')
 
 
-# In[52]:
+# In[ ]:
 
 
 
@@ -261,7 +262,7 @@ plt.xlabel('Hub height [m]')
 
 # #### First, here is a complicated way to find hte maximum of all arrays so all heatmaps are referenced to that value
 
-# In[73]:
+# In[10]:
 
 
 maxmax = 0
@@ -312,7 +313,7 @@ for hh in range (0, len(hub_heights)):
 print("MAX Found", maxmax)
 
 
-# In[77]:
+# In[11]:
 
 
 for hh in range (0, len(hub_heights)):
