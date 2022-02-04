0854dcdc2aef13c962192096706d15ba7f8300b7
cdeline <chris.deline@nrel.gov>
2022-02-04 08:52:57 -0700

    try python 3.6, 3.7, 3.8, 3.9 with updated requirements.txt

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
 
-    description='Tools to interface with Radiance',
+    description='Tools to interface with Radiance for the PV researcher',
     long_description=long_description,
     long_description_content_type="text/markdown",
 
@@ -77,7 +77,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # What does your project relate to?
