5fdc7373f7a58608070f3b83ac25abbfb25d81f8
Silvana Ovaitt <silvana.ayala@nrel.gov>
2022-08-03 05:13:29 -0600

    oregon, UL

--- docs/tutorials/(INSPIRE) AgriPV Oregon Site.py
+++ docs/tutorials/(INSPIRE) AgriPV Oregon Site.py
@@ -38,97 +38,78 @@ import numpy as np
 import datetime
 
 
-# In[8]:
+# In[3]:
 
 
 lat = 44.57615187732146
 lon = -123.23914850912513
 tilt = 25 # degrees
 sazm = 180 # degrees (south)
-clearance_height = 0.8805418 # m
-cw = 3.3655 # m
-pitch = 6.223  # m 
-hub_height =  1.4774418 # m
 numpanels = 2
-y = cw/2
-print("Panel length" , y)
+albedo = 0.2  #'grass'     # ground albedo
+
+# Three sites differences:
+cws = [3.3655, 3.3655, 3.9624] # m
+clearance_heights = [0.8805418, 0.9482582, 0.6985] # m
+pitchs = [6.223, 8.4201, 6.8453]  # m 
+
+ys = np.divide(cws,numpanels)
+print("Panel length" , ys)
 x = 1
 
 
-# In[14]:
+# In[5]:
 
 
-startdt =datetime.datetime(2021,5,1,0)
-enddt = datetime.datetime(2021,9,30,23)
-
-
-# In[6]:
-
-
-albedo = 0.2  #'grass'     # ground albedo
-demo = RadianceObj('OregonA', path=testfolder)  # Create a RadianceObj 'object'
+demo = RadianceObj('Oregon', path=testfolder)  # Create a RadianceObj 'object'
 demo.setGround(albedo) # input albedo number or material name like 'concrete'.  To see options, run this without any input.
 epwfile = demo.getEPW(lat, lon) # NJ lat/lon 40.0583° N, 74.4057
+metdata = demo.readWeatherFile(epwfile, starttime=startdt, endtime=enddt, coerce_year=2021) # read in the EPW weather data from above
+startdt =datetime.datetime(2021,5,1,0)
+enddt = datetime.datetime(2021,9,30,23)
+metdata = demo.readWeatherFile(epwfile, starttime=startdt, endtime=enddt, coerce_year=2021) # read in the EPW weather data from above
+demo.genCumSky()
 
 
 # ## 1. Loop over the different heights
 
-# In[10]:
+# In[14]:
 
 
-moduletype='PV-module'
-module = demo.makeModule(name=moduletype, x=x, y=y, numpanels=numpanels)
+for ii in range(0,3):
+    y= ys[ii]
+    ch = clearance_heights[ii]
+    pitch = pitchs[ii]
+    
+    moduletype='PV-module'
+    module = demo.makeModule(name=moduletype, x=x, y=y, numpanels=numpanels)
+    sceneDict = {'tilt':tilt, 'pitch':pitch, 'clearance_height':ch, 'azimuth':sazm, 'nMods':20, 'nRows':7}  
+    #starttime = '01_13_11';  endtime = '01_13_12'
+    scene = demo.makeScene(module=moduletype, sceneDict=sceneDict) #makeScene creates a .rad file with 20 modules per row, 7 rows.
+    octfile = demo.makeOct()  # makeOct combines all of the ground, sky and object fil|es into a .oct file.
+    # Sensor calculation
+    spacingbetweensamples = 0.05  # one sensor every 5 cm
+    sensorsy = round(pitch/spacingbetweensamples) -1 # one sensor every 5 cm
 
+    analysis = AnalysisObj(octfile, demo.name)  # return an analysis object including the scan dimensions for back irradiance
+    frontscan, backscan = analysis.moduleAnalysis(scene, sensorsy=[1, 9])
+    groundscan = frontscan.copy()
+    groundscan['zstart'] = 0.05  # setting it 5 cm from the ground.
+    groundscan['zinc'] = 0   # no tilt necessary. 
+    groundscan['yinc'] = spacingbetweensamples   # increasing spacing so it covers all distance between rows
+    groundscan['Ny'] = sensorsy   # increasing spacing so it covers all distance between rows
 
-# In[15]:
-
-
-startdt
-
-
-# In[16]:
-
-
-sceneDict = {'tilt':tilt, 'pitch':pitch, 'hub_height':hub_height, 'azimuth':sazm, 'nMods':20, 'nRows':7}  
-#starttime = '01_13_11';  endtime = '01_13_12'
-scene = demo.makeScene(module=moduletype, sceneDict=sceneDict) #makeScene creates a .rad file with 20 modules per row, 7 rows.
-octfile = demo.makeOct(demo.getfilelist())  # makeOct combines all of the ground, sky and object fil|es into a .oct file.
-metdata = demo.readWeatherFile(epwfile, starttime=startdt, endtime=enddt, coerce_year=2021) # read in the EPW weather data from above
-
-
-# In[17]:
-
-
-demo.genCumSky()
-octfile = demo.makeOct()  # makeOct combines all of the ground, sky and object files into a .oct file.
-
-
-# In[27]:
-
-
-# Sensor calculation
-spacingbetweensamples = 0.05  # one sensor every 5 cm
-sensorsy = round(pitch/spacingbetweensamples) -1 # one sensor every 5 cm
-
-
-# In[33]:
-
-
-analysis = AnalysisObj(octfile, demo.name)  # return an analysis object including the scan dimensions for back irradiance
-frontscan, backscan = analysis.moduleAnalysis(scene, sensorsy=[1, 9])
-groundscan = frontscan.copy()
-groundscan['zstart'] = 0.05  # setting it 5 cm from the ground.
-groundscan['zinc'] = 0   # no tilt necessary. 
-groundscan['yinc'] = spacingbetweensamples   # increasing spacing so it covers all distance between rows
-groundscan['Ny'] = sensorsy   # increasing spacing so it covers all distance between rows
-
-analysis.analysis(octfile, '_A_Module_', frontscan, backscan)  # compare the back vs front irradiance  
-analysis.analysis(octfile, '_A_Ground_', frontscan, groundscan)  # compare the back vs front irradiance  
+    analysis.analysis(octfile, 'Site_'+str(ii)+'_Module_', frontscan, backscan)  # compare the back vs front irradiance  
+    analysis.analysis(octfile, 'Site_'+str(ii)+'_Ground_', frontscan, groundscan)  # compare the back vs front irradiance  
 
 
 # ## 2. Plot Bifacial Gain Results
 
-# In[13]:
+# ## 3. Plot Heatmaps of the Ground Irradiance
+
+# #### First, here is a complicated way to find the maximum of all arrays so all heatmaps are referenced to that value
+
+# In[12]:
 
 
 import pandas as pd
@@ -137,7 +118,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 
-# In[14]:
+# In[46]:
 
 
 font = {'family' : 'normal',
@@ -145,32 +126,63 @@ font = {'family' : 'normal',
         'size'   : 22}
 matplotlib.rc('font', **font)
 
-sns.set(rc={'figure.figsize':(11.7,8.27)})
+sns.set(rc={'figure.figsize':(5,5)})
 
 
-# In[15]:
+# In[33]:
 
 
-hub_heights = [4.3, 3.5, 2.5, 1.5]
 results_BGG=[]
-for i in range(0, len(hub_heights)):
-    hub_height = str(int(hub_heights[i]*100))
-    filepv= os.path.join(testfolder,'results',f'irr_height_{hub_height}cm_PV_6.csv')
-    resultsDF = load.read1Result(filepv)
-    resultsDF = load.cleanResult(resultsDF).dropna()
-    results_BGG.append(resultsDF['Wm2Back'].sum()*100/resultsDF['Wm2Front'].sum())
+results_G=[]
+results_coords= []
+for ii in range(0, 3):
+    filepvf= os.path.join(testfolder,'results',f'irr_Site_'+str(ii)+'_Module__Row4_Module10_Front.csv')
+    filepvb= os.path.join(testfolder,'results',f'irr_Site_'+str(ii)+'_Module__Row4_Module10_Back.csv')
+    fileground= os.path.join(testfolder,'results',f'irr_Site_'+str(ii)+'_Ground__Row4_Module10_Back.csv')
+    resultsDF = load.read1Result(filepvf)
+    resultsDB = load.read1Result(filepvb)
+    resultsG = load.read1Result(fileground)
+    #resultsDF = load.cleanResult(resultsDF).dropna()
+    results_BGG.append(np.round(resultsDB['Wm2Back'].mean()*100/resultsDF['Wm2Front'].mean(),1))
+    results_G.append(resultsG['Wm2Back'])
+    results_coords.append(resultsG['y'])
     
+
+
+# In[27]:
+
+
+GHI_season = demo.metdata.ghi.sum()
+
+
+# In[28]:
+
+
+print("Bifacial Gain Results for Each site", results_BGG)
+
+
+# In[48]:
+
+
+plt.figure(figsize=(7,7))
+plt.plot(results_coords[0],results_G[0]/GHI_season, label='Site 1')
+plt.plot(results_coords[1],results_G[1]/GHI_season, label='Site 2')
+plt.plot(results_coords[2],results_G[2]/GHI_season, label='Site 3')
+plt.legend()
+plt.ylabel("Irradiance Factor on Ground")
+plt.xlabel("Pitch [m]")
+
+
+# In[ ]:
+
+
 plt.figure(figsize=(14,10))
 plt.plot(hub_heights, results_BGG, '.-')
 plt.ylabel('Bifacial Gain in Irradiance (BG$_G$) [%]')
 plt.xlabel('Hub height [m]')
 
 
-# ## 3. Plot Heatmaps of the Ground Irradiance
-
-# #### First, here is a complicated way to find the maximum of all arrays so all heatmaps are referenced to that value
-
-# In[16]:
+# In[ ]:
 
 
 maxmax = 0
@@ -223,7 +235,7 @@ print("MAX Found", maxmax)
 
 # Now let's print Results Table and Ground Irradiance Heatmaps:
 
-# In[23]:
+# In[ ]:
 
 
 for hh in range (0, len(hub_heights)):
