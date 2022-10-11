55b885f533795d5409b7a20faaa2ec470601086c
Silvana Ovaitt <silvana.ayala@nrel.gov>
2022-10-11 07:28:02 -0600

    modelchain Debug Journal

--- docs/tutorials/(dev) Modelchain test.py
+++ docs/tutorials/(dev) Modelchain test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[5]:
 
 
 import bifacial_radiance
@@ -9,39 +9,40 @@ import os
 import pandas as pd
 
 
-# In[2]:
+# In[9]:
 
 
 #inifile = r'C:\Users\sayala\Documents\GitHub\bifacial_radiance\tests\simulationView.ini'
 #inifile = r'C:\Users\sayala\Documents\GitHub\bifacial_radiance\tests\ini_highAzimuth.ini'
-inifile = r'C:\Users\sayala\Documents\GitHub\bifacial_radiance\tests\simulation3.ini'
+#inifile = r'C:\Users\sayala\Documents\GitHub\bifacial_radiance\tests\simulation3.ini'
+inifile = r'C:\Users\sayala\Documents\GitHub\bifacial_radiance\tests\simulation_T01.ini'
 
 
-# In[3]:
+# In[10]:
 
 
 (Params)= bifacial_radiance.load.readconfigurationinputfile(inifile=inifile)
 
 
-# In[4]:
+# In[8]:
 
 
 demo2, analysis = bifacial_radiance.modelchain.runModelChain(*Params ) 
 
 
-# In[5]:
+# In[ ]:
 
 
 get_ipython().system('rvu -vf views\\side.vp -e .01 1axis_2021-06-17_1300.oct')
 
 
-# In[6]:
+# In[ ]:
 
 
 demo2.CompiledResults
 
 
-# In[7]:
+# In[ ]:
 
 
 demo2.trackerdict
