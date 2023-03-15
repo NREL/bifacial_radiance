d432ae04b7a7c71fb65deaa34435679e18428b43
cdeline <chris.deline@nrel.gov>
2023-03-15 14:08:46 -0600

    pytests for mismatch_fit3 with new mad_fn changes

--- bifacial_radiance/mismatch.py
+++ bifacial_radiance/mismatch.py
@@ -163,6 +163,45 @@ def calculatePVMismatch(pvsys, stdpl, cellsx, cellsy, Gpoat):
 
     return PowerAveraged, PowerDetailed
 
+def mismatch_fit3(data):
+    '''
+    Electrical mismatch calculation following Progress in PV paper
+    Estimating and parameterizing mismatch power loss in bifacial photovoltaic systems
+    Chris Deline, Silvana Ayala Pelaez,Sara MacAlpine,Carlos Olalla
+    https://doi.org/10.1002/pip.3259 
+    
+    Parameters
+    ----------
+    data : np.ndarray, pd.Series, pd.DataFrame
+        Gtotal irradiance measurements. Each column is the irradiance for a module
+        at a specific time. 
+
+    Returns
+    -------
+    fit3 :  Float or pd.Series
+        Returns mismatch values for each module
+    
+    Equation: 1/(n^2*Gavg)*Sum Sum (abs(G_i - G_j))
+    ## Note: starting with Pandas 1.0.0 this function will not work on Series objects.
+    '''
+    import numpy as np
+    import pandas as pd
+    
+    if type(data) == np.ndarray:
+        data = pd.DataFrame(data)
+    
+    datac = data[~np.isnan(data)]
+    mad = mad_fn(datac, axis=1) /100  # (percentage)
+    mad2 = mad**2
+    
+    fit3 = 0.054*mad + 0.068*mad2
+    
+    if fit3.__len__() == 1:
+        fit3 = float(fit3)
+
+    return fit3
+
+    
 def mad_fn(data, axis='index'):
     '''
     Mean average deviation calculation for mismatch purposes.
